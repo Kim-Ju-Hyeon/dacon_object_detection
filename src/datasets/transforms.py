94d32213589ea783e3069c06359ac9988d4d1a78
KimJuHyeon <wngus1310@naver.com>
2023-05-25 13:36:45 +0900

    Add GridSearch Transform function

--- src/datasets/transforms.py
+++ src/datasets/transforms.py
@@ -1,5 +1,5 @@
 
-import cv2
+import itertools
 import albumentations as A
 from albumentations.pytorch.transforms import ToTensorV2
 
@@ -20,3 +20,19 @@ def get_test_transforms(img_size=512):
         A.Resize(img_size, img_size),
         ToTensorV2(),
     ])
+
+def grid_search_train_transforms(augmentation_config, img_size=512):
+    all_augmentations = []
+    for aug_name, params in augmentation_config.items():
+        AugClass = getattr(A, aug_name)
+        augmentation = AugClass(**params)
+        all_augmentations.append([None, augmentation])  # Either no augmentation or the augmentation
+
+    for augmentations in itertools.product(*all_augmentations):
+        augmentations = [aug for aug in augmentations if aug is not None]  # Remove None values
+        name = ['_'+str(aug).split('(')[0] for aug in augmentations if aug is not None]
+        augmentations.append(A.Resize(img_size, img_size))
+        augmentations.append(A.Normalize())
+        augmentations.append(ToTensorV2())
+
+        yield A.Compose(augmentations, bbox_params=A.BboxParams(format='pascal_voc', label_fields=['labels'])), ''.join(name)
